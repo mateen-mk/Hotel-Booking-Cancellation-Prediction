50febcfdaad1dccbf7e436338dc0401f57d78886
Mateen Khan <mateenmk2428@gmail.com>
2024-11-07 10:59:50 +0530

    Dependencies Installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'hotel_booking_cancellation',
+    version = '1.0.0',
+    packages = find_packages(),
+    include_package_data = True,
+    description = 'Predicting hotel booking cancellations',
+    author = 'Mateen Khan',
+    author_email = 'mateenmk24287@gmail.com'
+)
